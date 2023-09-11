d634e1a9e12f08659c20fbfe85cd0f04569fc385
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-09-11 18:06:38 -0400

    Testing with main card component

---
+++ src/Components/ArtCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArtCard = ({link}) => {
+    console.log(link)
+    return (
+        <div>
+        <img src={link} alt="" />
+        </div>
+)}
+
+export default ArtCard
---
+++ src/api/fetchArt.js
@@ -0,0 +1,3 @@
+export function getSingleArt(id){
+    const art = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+}
