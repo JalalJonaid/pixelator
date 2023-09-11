93bc661f0eca729093fb7a505bb00cb1b06358b7
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-09-11 19:33:42 -0400

    new fetch added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import ArtCard from "./Components/artCard";
 import { getSingleArt } from "./api/fetchArt";
+import { getValidId } from "./api/fetchArt";
 
 function App() {
   const [link, setLink] = useState("");
 
-  useEffect(() => {
+  const getID = () => {
     try {
-      getSingleArt(439)
+      getValidId()
       .then((data) => data.json())
       .then((res) => {
-        console.log(res);
+        console.log(res.objectIDs[4]);
+        return res.objectIDs[4]
+      });
+    } catch(err) {
+      console.error('Error happening!', err)
+    }
+  }
+console.log(getID())
+  useEffect(() => {
+    try {
+      getSingleArt(getID())
+      .then((data) => data.json())
+      .then((res) => {
         setLink(res.primaryImage);
       });
     } catch(err) {
