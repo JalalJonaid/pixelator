954a2eceef10dedb2a9f4824998e6c33cd1749d7
Genesis Lara <genesislara@pursuit.org>
2023-09-13 19:45:45 -0400

    adds to artcard and edits typo in Home file

--- src/Components/ArtCard.jsx
+++ src/Components/ArtCard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 const ArtCard = ({ link }) => {
-  console.log(link);
+
   return (
-    <div>
+    <div className="artCard">
       <img src={link} alt="" height={"400px"} width={"400px"} />
+      <h3>Title PlaceHolder:</h3>
+      <p> Creator: </p>
+      <p> Year created: </p>
+      <p> Collection: </p>
+      <p> Generate pixel button? </p>
     </div>
   );
 };
