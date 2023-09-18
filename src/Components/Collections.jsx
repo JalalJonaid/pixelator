89c5f3c24ecdc2556fa8101d68c30b1fa7bd5424
Shanice <shanicegriffin@pursuit.org>
2023-09-18 19:26:23 -0400

    collections by department working

--- src/Components/Collections.jsx
+++ src/Components/Collections.jsx
@@ -1,79 +1,96 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import departmentData from "../departmentData/data.json"
 
 const Collections = () => {
   const [artWorks, setArtWorks] = useState([]);
-  const [error, setError] = useState(null);
+  const [departmentIdInput, setDepartmentIdInput] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
 
-  useEffect(() => {
-    const fetchArtWorks = async () => {
-      try {
-        const departmentId = 1;
-        const response = await fetch(
-          `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departmentId}`
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (departmentIdInput === "" || departmentIdInput === "2") {
+      setErrorMessage(true);
+    } else {
+      fetchArtworksByDepartment(departmentIdInput);
+      form.reset();
+    }
+  };
+
+  const fetchArtworksByDepartment = async (departmentId) => {
+    try {
+      const response = await fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departmentId}`
+      );
+
+      const data = await response.json();
+      const objectIDs = data.objectIDs;
+      const validArtworks = [];
+
+      for (let i = 0; i < objectIDs.length && validArtworks.length < 20; i++) {
+        const objectID = objectIDs[i];
+        const objectResponse = await fetch(
+          `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`
         );
+        const objectData = await objectResponse.json();
 
-        if (!response.ok) {
-          throw new Error("Network response not ok!");
+        if (objectData.primaryImage) {
+          validArtworks.push(objectData);
         }
-
-        const data = await response.json();
-        const objectIDs = data.objectIDs;
-
-        const promises = [];
-        const validArtworks = [];
-
-        for (
-          let i = 0;
-          i < objectIDs.length && validArtworks.length < 20;
-          i++
-        ) {
-          const objectID = objectIDs[i];
-          const objectResponse = await fetch(
-            `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`
-          );
-          const objectData = await objectResponse.json();
-
-          if (objectData.primaryImage) {
-            validArtworks.push(objectData);
-          }
-        }
-
-        setArtWorks(validArtworks);
-      } catch (error) {
-        setError(error);
       }
-    };
-    fetchArtWorks();
-  }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+      setErrorMessage(false);
+      setArtWorks(validArtworks);
+    } catch (error) {
+      console.error(`Error fetching art pieces by department:`, error);
+    }
+  };
 
   return (
-    <div style={{ marginTop: "60px" }}>
+    <div className="collection" style={{ marginTop: "60px" }}>
       <h1>Artworks from the Department</h1>
-      <form>
-        <label for="">See sample art from a certain Department (enter a number btween 1 & 21): </label>
-        <input type="number" id="departmentId" name="departmentId" min={1} max={21} required/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="departmentId">
+          See sample art from a certain Department (enter a number between 1 &
+          21):
+        </label>
+        <input
+          type="number"
+          id="departmentId"
+          name="departmentId"
+          min={1}
+          max={21}
+          value={departmentIdInput}
+          onChange={(e) => setDepartmentIdInput(e.target.value)}
+        />
         <button type="submit" className="btn btn-danger">
           Search
         </button>
       </form>
-      <div className="artworks">
-        {artWorks.map((artwork) => (
-          <div key={artwork.objectID} className="artwork">
-            <img
-              src={artwork.primaryImage}
-              alt={artwork.title}
-              height="400px"
-              width="400px"
-            />
-            <h3>{artwork.title}</h3>
-            <p>{artwork.artistDisplayName}</p>
-            <p>{artwork.objectDate}</p>
+      <div className="error-message"> 
+        {errorMessage && (
+          <div className="alert alert-danger mt-3">
+            Something is wrong with the input, try again.
           </div>
-        ))}
+        )}
+      </div>
+      <div className="artworks ">
+        {artWorks && artWorks.length > 0 && (
+            <div>
+            {artWorks.map((artwork) => (
+              <div key={artwork.objectID} className="artwork">
+                <img
+                  src={artwork.primaryImage}
+                  alt={artwork.title}
+                  height="200px"
+                  width="200px"
+                />
+                <p>{artwork.title}</p>
+                <p>{artwork.artistDisplayName}</p>
+                <p>{artwork.objectDate}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
