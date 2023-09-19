20c5575b3698f86ddfb6f8c0b2ca1b97e02498a6
Shanice <shanicegriffin@pursuit.org>
2023-09-19 12:31:13 -0400

    fixed home page layout ad added css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef } from "react";
-import './Home.css'
-import {pixelit} from "../pixelit.js"
+import "./Home.css";
+import { pixelit } from "../pixelit.js";
 
 const Home = ({ link }) => {
   const canvasRef = useRef(null);
@@ -12,44 +12,60 @@ const Home = ({ link }) => {
   };
 
   return (
-    <div className="home-page" style={{ marginTop: "60px" }}>
-      <div className="art-card-container">
-      <button className="button-74" onClick={handleRefresh}>PIXEL-ATE</button>
-        {link ? (
-          <div className="artCard">
-            <img
-              id="pixelitimg"
-              ref={imageRef}
-              src={link.primaryImage}
-              style={{maxWidth: "200px", maxHeight: "auto"}}
-              onLoad={() => {
-                if (imageRef.current !== null) {
-                  const px = new pixelit({
-                    scale : 8,
-                    palette : [[154, 99, 72]],
-                    maxHeight: 400,
-                    maxWidth: 400,
-                  });
-                  px.draw().pixelate();
-                  imageRef.current.style.visibility = "visible"; //check w/ Tim
-                }
-              }}
-            />
-            <h3>{link.title}</h3>
-            <p>{link.artistDisplayName}</p>
-            <p>{link.objectDate}</p>
-          </div>
-        ) : (
-          <div className="loading_image">
-          <img src="../favicon/loading_image.png"></img>
-          </div>
-        )}
+    <div className="home-page" style={{ marginTop: "70px" }}>
+      <button className="button-74" onClick={handleRefresh}>
+        PIXEL-ATE
+      </button>
+      <div
+        className="art-card-container row row-cols-2"
+        style={{ marginTop: "60px" }}
+      >
+        <div>
+          {link ? (
+            <div className="artCard row row-cols-2">
+              <img
+                src={link.primaryImage}
+                alt={link.title}
+                height="400px"
+                width="400px"
+                className="original-img"
+              />
+              <img
+                id="pixelitimg"
+                ref={imageRef}
+                src={link.primaryImage}
+                style={{ maxWidth: "200px", maxHeight: "auto" }}
+                onLoad={() => {
+                  if (imageRef.current !== null) {
+                    const px = new pixelit({
+                      scale: 8,
+                      palette: [[154, 99, 72]],
+                      maxHeight: 400,
+                      maxWidth: 400,
+                    });
+                    px.draw().pixelate();
+                    // imageRef.current.style.visibility = "visible"; //check w/ Tim
+                  }
+                }}
+              />
+              <div className="details">
+                <h3>{link.title}</h3>
+                <p>{link.artistDisplayName}</p>
+                <p>{link.objectDate}</p>
+              </div>
+            </div>
+          ) : (
+            <div className="loading_image">
+              <img src="../favicon/loading_image.png"></img>
+            </div>
+          )}
+        </div>
+        <canvas
+          id="pixelitcanvas"
+          style={{ maxWidth: "325px", maxHeight: "auto" }}
+          ref={canvasRef}
+        ></canvas>
       </div>
-      <canvas
-        id="pixelitcanvas"
-        style={{maxWidth: "200px", maxHeight: "auto"}}
-        ref={canvasRef}
-      ></canvas>
     </div>
   );
 };
